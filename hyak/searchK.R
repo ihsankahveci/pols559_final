99c8d9ec5db10821034027bc2b8cf63d8b8ee577
İhsan Kahveci <ikahveci@uw.edu>
2021-02-04 04:16:44 +0300

    updated hyak

--- hyak/searchK.R
+++ hyak/searchK.R
@@ -3,23 +3,22 @@ library(readr)
 
 
 sermons = read_csv("../sermons_final.csv")
-
+#sermons = read_csv("sermons_final.csv")
 
 #preprocessing the text
 processed <- textProcessor(sermons$translated, metadata=sermons, 
-                           language = "en", stem = F) #citation 
+                           language = "en", stem = T) #citation 
 
 out <- prepDocuments(processed$documents, processed$vocab, processed$meta,
                      upper.thresh = 100, lower.thresh = 20)
 docs <- out$documents
 vocab <- out$vocab
 meta  <- out$meta
-meta$date <- as.numeric(meta$date) - min(as.numeric(meta$date))
 
 #finding the optimal number of K
 storage <- searchK(docs, vocab, data = meta,
-                   K = 3:50,  
-                   prevalence = ~s(date) + casualty + price + ramadan + sacrifice,  
+                   K = 3,  
+                   prevalence = ~turkish + kurdish + price + location + ramadan + sacrifice,  
                    seed = 57, cores=8,
                    verbose=T)
 
